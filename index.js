adbeb6bcabc43b005d8c6447932a974733a71d13
IraTrukhan <iryna.viaz@gmail.com>
2021-10-18 15:01:23 +0300

    add button

---
+++ index.js
@@ -0,0 +1,9 @@
+let greeting = function(name, age, answer) {
+    alert('Hello!\nMy name ' + name 
+    + '\nI\'m ' + age 
+    + '\nDo you like learning Front End?\n' + answer);
+   }
+let myName = 'Iryna';
+let age = 32;
+let answer = 'Yes';
+   
